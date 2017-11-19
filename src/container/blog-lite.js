87f72bf234ca075d0fbe9302ce745de1f48ef585
elukuro <heruhartantot@gmail.com>
2017-11-19 07:18:06 +0700

    blog lite container

--- src/container/blog-lite.js
+++ src/container/blog-lite.js
@@ -5,13 +5,16 @@ import {connect} from 'react-redux';
 import _ from 'lodash';
 import Loading from './loading';
 
-class Blog extends Component{
+class BlogLite extends Component{
 
 	constructor(props){
 		super(props);
 		this.state={
 			blog:{},
-			loading:true
+			loading:true,
+			heading:"Here my latest note that I write",
+			quote:"sometime I stop and back to one of this note to remaind me how great my life is",
+			text:{}
 		}
 	}
 
@@ -19,10 +22,24 @@ class Blog extends Component{
 		axios.get('http://goheru.com/public/postJson/all').then((response)=> {
 				this.setState({blog:response.data,loading:false});
  		 })
+
+		axios.get('http://goheru.com/public/landingpageJson').then((response)=> {
+    			this.setState({text:response.data});
+ 		 })
 	}
 
+	renderText(){
+		return _.map(this.state.text,item=>{
+			return(
+				<p className="text" key={item.id}>{item.content_admin}</p>		
+			)
+		})
+	}
+
+
 	renderBlog(){
-		return _.map(this.state.blog,item=>{
+
+		return _.map(this.state.blog.slice(0,3),item=>{
 			return(
 				<li key={item.id}>
 					<p className="text">{item.created_at} #{item.category}</p>
@@ -40,15 +57,20 @@ class Blog extends Component{
 			)
 		}else{
 			return(
-				<div className="blog">
+				<div className="blog blog-lite">
 					<div className="blog-wrap">
+					<h2 className="heading">{this.state.heading}</h2>
+					<p className="text">{this.state.quote}</p>
 						<div className="blog-list">
 							<ul>
 								{this.renderBlog()}
 							</ul>
 						</div>
 					</div>
-					
+					<div className="center">
+					<img src={require('../images/plant.png')} className="image"/>
+					<p className="right text">created 2017</p>
+					</div>
 				</div>
 			)
 		}
@@ -60,4 +82,4 @@ class Blog extends Component{
 // 	return{landingpage:state.posts}
 // }
 
-export default connect(null,{})(Blog);
+export default connect(null,{})(BlogLite);
