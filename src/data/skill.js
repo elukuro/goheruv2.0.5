43975968dc3976a8c75b43ebcd5841a6ec88fe3e
elukuro <heruhartantot@gmail.com>
2020-05-09 10:28:00 +0700

    update skill

--- src/data/skill.js
+++ src/data/skill.js
@@ -1,17 +1,16 @@
 const skill = [
   { id: 2, description: "HTML & CSS , Sass", point: 40, category: 0 },
-  { id: 3, description: "Javascript , Jquery", point: 40, category: 0 },
-  { id: 6, description: "Sublime text", point: 35, category: 1 },
-  { id: 7, description: "Gulp.js", point: 40, category: 1 },
-  { id: 8, description: "webpack 2", point: 30, category: 1 },
-  { id: 9, description: "Git", point: 40, category: 1 },
-  { id: 10, description: "Simple web design", point: 20, category: 2 },
+  { id: 3, description: "Javascript", point: 40, category: 0 },
   { id: 11, description: "PHP ", point: 35, category: 0 },
-  { id: 12, description: "React", point: 42, category: 0 },
-  { id: 13, description: "Sketch", point: 20, category: 2 },
+  { id: 12, description: "React", point: 45, category: 0 },
+  { id: 16, description: "Vue", point: 45, category: 0 },
+  { id: 7, description: "Gulp, Webpack", point: 40, category: 1 },
+  { id: 8, description: "Eslint, Jest", point: 30, category: 1 },
+  { id: 9, description: "Git", point: 40, category: 1 },
   { id: 15, description: "VS code", point: 40, category: 1 },
-  { id: 16, description: "Vue.js", point: 35, category: 0 }
+  { id: 6, description: "Sublime text", point: 35, category: 1 },
+  { id: 13, description: "Sketch", point: 20, category: 2 },
+  { id: 10, description: "Simple web design", point: 40, category: 2 },
 ];
-
 
 export default skill
